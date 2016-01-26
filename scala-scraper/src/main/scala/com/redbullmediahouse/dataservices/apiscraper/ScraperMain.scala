45474969fc9a4df7aef042e82289acc8850303b2
lverzosa <lorenzv@gmail.com>
2016-01-26 11:16:08 -0800

    Added scala rest api client example

---
+++ scala-scraper/src/main/scala/com/redbullmediahouse/dataservices/apiscraper/ScraperMain.scala
@@ -0,0 +1,17 @@
+package com.redbullmediahouse.dataservices.apiscraper
+
+import scala.io.Source
+import scala.util.parsing.json.JSON
+
+/**
+  * Created by lverzosa on 1/26/16.
+  */
+object ScraperMain {
+  def main(args: Array[String]) {
+    val uri:String = "https://api.github.com/repos/lverzosa/scala-data-scraper"
+    val result:String = Source.fromURL(uri).mkString
+    val json:Option[Any] = JSON.parseFull(result)
+
+    println(json.get)
+  }
+}
