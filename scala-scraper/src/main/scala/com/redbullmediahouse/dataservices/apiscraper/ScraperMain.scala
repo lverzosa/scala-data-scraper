b304060e955d509d851761e2a309c9dfd31fd348
lverzosa <lorenzv@gmail.com>
2016-01-26 14:14:28 -0800

    Changed to spray instead of built in scala (since scala json parsing is not included in 2.10).

--- scala-scraper/src/main/scala/com/redbullmediahouse/dataservices/apiscraper/ScraperMain.scala
+++ scala-scraper/src/main/scala/com/redbullmediahouse/dataservices/apiscraper/ScraperMain.scala
@@ -2,16 +2,24 @@ package com.redbullmediahouse.dataservices.apiscraper
 
 import scala.io.Source
 import scala.util.parsing.json.JSON
+import spray.json._
+import DefaultJsonProtocol._
 
 /**
   * Created by lverzosa on 1/26/16.
   */
 object ScraperMain {
   def main(args: Array[String]) {
-    val uri:String = "https://api.github.com/repos/lverzosa/scala-experiments"
+    val uri:String = "https://api.github.com/repos/lverzosa/scala-experiments/branches"
     val result:String = Source.fromURL(uri).mkString
     val json:Option[Any] = JSON.parseFull(result)
 
-    println(json.get)
+    //println(json.get)
+
+    //val resultArray:List[Map[String, Any]] = json.get.asInstanceOf[List[Map[String, Any]]]
+
+    //resultArray.foreach( (x:Map[String, Any]) => println(x.getOrElse("name","MISSING_NAME")) )
+
+    println(result.parseJson)
   }
 }
